13f164af895c14e571a9182745acd143a6acee55
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-06-09 16:18:51 +0545

    bug fixed

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,21 +2,19 @@ class TodoController < ApplicationController
    
     def index
         @todo  = Todolist.all
+        
     end
 
     def show
         @todo = Todolist.find(params[:id])
-        # redirect_to show_todo_path
     end
 
     def new
         @todo = Todolist.new
-
     end
 
 
     def create 
-        puts "hello world"
         @todo = Todolist.new(params.require(:todo).permit(:description))
         if @todo.save
             redirect_to todo_index_path
@@ -27,8 +25,6 @@ class TodoController < ApplicationController
 
     def update 
         @todo = Todolist.find(params[:id]).update(params.require(:todo).permit(:description))
-
-
         if @todo
             redirect_to todo_index_path
         else
@@ -40,10 +36,10 @@ class TodoController < ApplicationController
     def edit
         @todo = Todolist.find(params[:id])
     end
+
     def destroy
-        puts params[:id], Todolist
-            @todo=Todolist.find(params[:id]).delete
-            redirect_to todo_index_path
+        @todo=Todolist.find(params[:id]).delete
+        redirect_to todo_index_path
     end
     
 
