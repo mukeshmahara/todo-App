434bc25a05b0fe02f9096af2837d78c3b3dbfab6
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-06-09 13:27:22 +0545

    imported bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,23 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+require("bootstrap")
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+require("bootstrap")
+import "../stylesheets/application";
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
+
+
 
 Rails.start()
 Turbolinks.start()
