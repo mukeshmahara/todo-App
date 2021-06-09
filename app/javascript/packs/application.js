de687af46c1856877fff1a9bb0c3da63d5459287
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-06-09 23:00:19 +0545

    added fontawesome and tooltips

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@
 
 
 require("bootstrap") //requiring bootstrap
+import "@fortawesome/fontawesome-free/css/all" //importing fontawesome
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
